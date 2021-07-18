6e29e70d885b86c132e5d6f86292864d40d8564a
user123 <mo7amedfares321@gmail.com>
2021-07-18 17:58:07 +0200

    all good

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 3000
 app.get("/",(req,resp)=>{
     resp.json("hey")
 })
-app.listen(3000)
+app.listen(PORT)
 
 
